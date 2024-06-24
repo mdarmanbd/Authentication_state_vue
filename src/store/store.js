1a6328a23a1b9eff704830cdc75e57a06c57215d
arman_hossain <md.armantext@gmail.com>
2024-06-24 23:04:16 +0600

    clean a project

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,7 @@
 import{reactive, ref} from 'vue'
 
 const store = reactive({
-    demoCustomer : true,
-    findCustomerIndex : 0,
-    isModalOpen : false,
-     coustomerView(index){
-        this.findCustomerIndex = index
-    },
-    login(){
-        this.isModalOpen = true
-    }
+
     
 
 })
